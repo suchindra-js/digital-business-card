a0d95aef96bc7f3ecc89f2e43e6c22458cdbced4
Suchindra <69780675+suchindra-js@users.noreply.github.com>
2022-02-21 21:18:04 +0800

    final commit

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -2,7 +2,7 @@ export default function Interests() {
     return(
     <div className="interests-container">
         <h3 className="interests--title">Interests</h3>
-        <p className="interests--para">I am a web developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        <p className="interests--para">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat aspernatur consectetur voluptas autem inventore vitae repudiandae.</p>
     </div>
     )
 }
